d755064db5cffb5760eb4fded4bf8799f29f18f0
BlurryQ <skullsmash@hotmail.co.uk>
2024-01-09 20:57:07 +0000

    Style function working on header

--- script.js
+++ script.js
@@ -34,16 +34,16 @@ function removeUnderline(id) {
 
 const links = document.querySelectorAll("a");
 links.forEach(link => {
-    link.addEventListener("mouseover", () => {
+    link.addEventListener("mouseenter", () => {
         const linkID = link.id
         addBorder(linkID)
     })    
 });
 
 links.forEach(link => {
-    link.addEventListener("mouseout", () => {
+    link.addEventListener("mouseleave", () => {
         const linkID = link.id
-        if(link.id) { removeBorder(linkID)}
+        if(link.id) { removeBorder(linkID) }
     })    
 });
 
@@ -54,18 +54,27 @@ function addBorder(id) {
     const icon = document.getElementById(iconID)
     const content = document.getElementById(id)
 
-    icon.style.cssText = `  border: 3px solid var(--flair-color);
-                            border-radius: 15px;`
-    content.style.cssText = `   text-decoration: underline;
-                                text-decoration-color: var(--flair-color);
-                                text-decoration-thickness: 3px;`
+    icon.classList.remove("removeMovingBorder")
+    icon.classList.add("addMovingBorder")
 
 }
 
+function removeBorder(id) {
+    const ids = id.split("-")
+    console.log("ids: " + ids)
+    const iconID = id + "-icon"
+    const icon = document.getElementById(iconID)
+    const content = document.getElementById(id)
 
+    icon.classList.remove("addMovingBorder")
+    icon.classList.add("removeMovingBorder")
 
+}
 
-
+function idSplit(ids) {
+    
+}
+/* 
 const home = document.getElementById("home-icon");
 home.addEventListener("mouseover", () => {
     home.style.cssText = `  border: 3px solid var(--flair-color);
@@ -74,4 +83,4 @@ home.addEventListener("mouseover", () => {
 
 home.addEventListener("mouseout", () => {
     home.style.cssText = ""
-})
+}) */
