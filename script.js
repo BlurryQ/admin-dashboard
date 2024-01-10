c90db41d5f5630a1b54aff84adb0abe6d0e81bff
BlurryQ <skullsmash@hotmail.co.uk>
2024-01-10 17:08:42 +0000

    Finished JS style funcitons

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function addUnderline(id) {
 function removeUnderline(id) {
     const blockToChange = document.getElementById(id);
     const contentID = id + "-content"
-    const contentToChange = document.getElementById(contentID);
     blockToChange.style.cssText = ""
 }
 
@@ -36,51 +35,67 @@ const links = document.querySelectorAll("a");
 links.forEach(link => {
     link.addEventListener("mouseenter", () => {
         const linkID = link.id
-        addBorder(linkID)
+        if(linkID != "") { addBorder(linkID) }
     })    
 });
 
 links.forEach(link => {
     link.addEventListener("mouseleave", () => {
         const linkID = link.id
-        if(link.id) { removeBorder(linkID) }
+        if(linkID != "") { removeBorder(linkID) }
     })    
 });
 
 function addBorder(id) {
-    const ids = id.split("-")
-    console.log("ids: " + ids)
-    const iconID = id + "-icon"
-    const icon = document.getElementById(iconID)
-    const content = document.getElementById(id)
+    if(id === "dashboard-icon" || id === "dashboard") {
+        const splitIDs = id.split("-");
+        const content = document.getElementById(splitIDs[0]);
+        content.classList.add("addUnderline");
+        return;
+    }
+    const containsDash = id.includes("-")
+    if(containsDash) {
+        const splitIDs = id.split("-")
+        const icon = document.getElementById(id)
+        const content = document.getElementById(splitIDs[0])
+    
+        icon.classList.remove("removeMovingBorder")
+        icon.classList.add("addMovingBorder")
+        content.classList.add("addUnderline")
+    } else {
+        const iconID = id + "-icon"
+        const icon = document.getElementById(iconID)
+        const content = document.getElementById(id)
 
-    icon.classList.remove("removeMovingBorder")
-    icon.classList.add("addMovingBorder")
-
+        icon.classList.remove("removeMovingBorder")
+        icon.classList.add("addMovingBorder")
+        content.classList.add("addUnderline")
+    }
 }
 
 function removeBorder(id) {
-    const ids = id.split("-")
-    console.log("ids: " + ids)
-    const iconID = id + "-icon"
-    const icon = document.getElementById(iconID)
-    const content = document.getElementById(id)
+    if(id === "dashboard-icon" || id === "dashboard") {
+        const splitIDs = id.split("-");
+        const content = document.getElementById(splitIDs[0]);
+        content.classList.remove("addUnderline");
+        return;
+    }
+    const containsDash = id.includes("-")
+    if(containsDash) {
+        const splitIDs = id.split("-")
+        const icon = document.getElementById(id)
+        const content = document.getElementById(splitIDs[0])
 
-    icon.classList.remove("addMovingBorder")
-    icon.classList.add("removeMovingBorder")
+        icon.classList.remove("addMovingBorder")
+        icon.classList.add("removeMovingBorder")
+        content.classList.remove("addUnderline")
+    } else {
+        const iconID = id + "-icon"
+        const icon = document.getElementById(iconID)
+        const content = document.getElementById(id)
 
+        icon.classList.remove("addMovingBorder")
+        icon.classList.add("removeMovingBorder")
+        content.classList.remove("addUnderline")
+    }
 }
-
-function idSplit(ids) {
-    
-}
-/* 
-const home = document.getElementById("home-icon");
-home.addEventListener("mouseover", () => {
-    home.style.cssText = `  border: 3px solid var(--flair-color);
-                            border-radius: 15px;`
-})
-
-home.addEventListener("mouseout", () => {
-    home.style.cssText = ""
-}) */
